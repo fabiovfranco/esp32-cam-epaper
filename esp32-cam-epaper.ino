62e50827c037d0f84a68baa59226b048f0a53933
Fábio Vilela <712020:80081d33-2c62-4d71-b2ef-9122833a3a6e@bots.bitbucket.org>
2023-05-08 09:56:55 -0300

    Primeira versao funcional

--- esp32-cam-epaper.ino
+++ esp32-cam-epaper.ino
@@ -1,188 +1,102 @@
-/*
-    based on W. Hoogervorst, based on based on the 'CameraWebServer' example from the Arduino IDE for the AI Thinker ESP32-CAM board
-    7.5 inch Waveshare e-paper B/W
-    resolution 800x480
-*/
+#define ENABLE_GxEPD2_GFX 0
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+// 4 for flash led or 33 for normal led
+#define LED_GPIO_NUM       4
+
+#define LED_LEDC_CHANNEL 2 //Using different ledc channel/timer than camera
+#define CONFIG_LED_MAX_INTENSITY 255
+
+#include <GxEPD2_BW.h>
+#include "bitmaps/Bitmaps800x480.h" // 7.5"  b/w
+
 #include "esp_camera.h"
-//#include <WiFi.h>
 #include <img_converters.h>
 
-// include library, include base class, make path known
-#include <GxEPD.h>
-#include <GxGDEW075T7/GxGDEW075T7.h>      // 7.5" b/w 800x480
-
-#include <GxGDEW075T7/BitmapExamples.h>
-
-// FreeFonts from Adafruit_GFX
-//#include <Fonts/FreeSansBold9pt7b.h>
-//#include <Fonts/FreeSansBold12pt7b.h>
-//#include <Fonts/FreeSansBold18pt7b.h>
-//#include <Fonts/FreeSans9pt7b.h>
-//#include <Fonts/FreeSans12pt7b.h>
-//#include <Fonts/FreeSans18pt7b.h>
-
-#include <GxIO/GxIO_SPI/GxIO_SPI.h>
-#include <GxIO/GxIO.h>
-
-const char* ssid = "FABIO ";
-const char* password = "1qaz3edc";
-
-// for SPI pin definitions see e.g.:
-// C:\Users\xxx\Documents\Arduino\hardware\espressif\esp32\variants\lolin32\pins_arduino.h
-
-//GxIO_Class io(SPI, /*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16); // arbitrary selection of 17, 16
-//GxEPD_Class display(io, /*RST=*/ 16, /*BUSY=*/ 4); // arbitrary selection of (16), 4
-
-SPIClass  epaper_SPI(HSPI); // define the SPI for the e-paper on the HSPI pins (CLK 14, MOSI 13, CS 15)
-/*
-  #define CS 15
-  #define DC 4
-  #define RST 2
-  #define BUSY 16
+/* Conexao do epaper:
+BUSY - ROXO    - IO12
+RST  - BRANCO  - IO2
+DC   - VERDE   - IO4
+CS   - LARANJA - IO15
+CLK  - AMARELO - IO14
+DIN  - AZUL    - IO13
+GND  - MARROM  - GND (proximo ao 3v3)
+VCC  - CINZA   - 3V3
 */
-GxIO_Class io(epaper_SPI, /*CS*/ 15, /*DC*/ 4, /*RST*/ 2);
-GxEPD_Class display(io, /*RST*/ 2,  /*BUSY*/ 16 );
-
-// Select camera model
-#define CAMERA_MODEL_AI_THINKER
-#include "camera_pins.h"
-
-#define WIFI_CONNECT_TIMEOUT_S 15
-
-// WiFiClient espClient;
-
-boolean alarmstate;
-boolean updatescreen = false;
-
-void setupLedFlash(int pin);
-void enable_led(bool en);
-const GFXfont* f;
+// hspi.begin(14, 12, 13, 15); // remap hspi for EPD (swap pins)(sck, miso, mosi, ss)
+//GxEPD2_BW<GxEPD2_750_T7, GxEPD2_750_T7::HEIGHT> display(GxEPD2_750_T7(/*CS=*/ 15, /*DC=*/ 4, /*RST=*/ 2, /*BUSY=*/ 16)); // GDEW075T7 800x480, EK79655 (GD7965)
+GxEPD2_BW<GxEPD2_750_YT7, GxEPD2_750_YT7::HEIGHT> display(GxEPD2_750_YT7(/*CS=*/ 15, /*DC=*/ 4, /*RST=*/ 2, /*BUSY=*/ 12)); // GDEY075T7 800x480, UC8179 (GD7965)
+SPIClass hspi(HSPI);
 
 void setup() {
   Serial.begin(115200);
   delay(5000); // wait ArduinoIDE
   Serial.setDebugOutput(true);
   Serial.println();
-  Serial.println("init 000");
-  Serial.println("[APP] memory");
-  show_memory();
+  Serial.println("setup");
 
-  // pinMode(14,INPUT_PULLUP);
-  // pinMode(15,INPUT_PULLUP); 
-  // Wire.begin(14,15);
-  pinMode(33, OUTPUT);
-  digitalWrite(33, HIGH);
-  digitalWrite(4, LOW);
-  Serial.println("pin mode started");
-  display.init(115200);
-  display.setRotation(2);
+  show_memory("[1] memory");
+
+  init_camera();
+
+  hspi.begin(14, 12, 13, 15); // remap hspi for EPD (swap pins)(sck, miso, mosi, ss)
+  display.epd2.selectSPI(hspi, SPISettings(4000000, MSBFIRST, SPI_MODE0));
+  Serial.println("hspi configured");
+
+  display.init(115200, true, 2, false);
+  Serial.println();
   Serial.println("display initialized");
 
-  // camera init
-  // init_camera();
+  ////////////////////////////
+  uint8_t * buf = NULL;
+  size_t buf_len = 0;
+  enable_led(false);
+  if(take_photo(&buf, &buf_len) == ESP_FAIL) {
+    Serial.println("Fail to take photo");
+    return;
+  }
+  
+  Serial.print("buffer length:");Serial.println(buf_len);
+  esp_err_t err = esp_camera_deinit();
+  if(err != ESP_OK) {
+    Serial.printf("Camera deinit failed with error 0x%x", err);
+  }
 
-  // setup_wifi();
+  if(buf_len > 0) {
+    display.setRotation(0);
+    drawBitmap(buf, buf_len);
+    Serial.println("image printed");
 
-  //Serial.println("display printed");
-
-  //Serial.print("Camera Ready! Use 'http://");
-  //Serial.print(WiFi.localIP());
-  //Serial.println("' to connect");
+    Serial.println("cleaning memory");
+    show_memory("[3] memory");
+    free(buf);
+    buf_len = 0;
+  }
+  
+  show_memory("[2] memory");
 }
 
 void loop() {
-  Serial.println("[1] memory");
-  show_memory();
-
-  // uint8_t * buf = NULL;
-  // size_t buf_len = 0;
-  // enable_led(false);
-  // if(take_photo(&buf, &buf_len) == ESP_FAIL) {
-  //   Serial.println("Fail to take photo");
-  //   return;
-  // }
-  
-  // Serial.println("[1.1] memory");
-  // show_memory();
-
-  // Serial.print("buffer length:");Serial.println(buf_len);
-  // esp_err_t err = esp_camera_deinit();
-  // if(err != ESP_OK) {
-  //   Serial.printf("Camera deinit failed with error 0x%x", err);
-  // }
-
-  // Serial.println("[2] memory");
-  // show_memory();
-
-  // if(buf_len > 0) {
-    display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
-  //   //delay(5000);
-  //   //display.drawBitmap(buf, sizeof(buf));
-    // drawBitmap(buf, buf_len);
-  //   Serial.println("image printed");
-
-    // Serial.println("cleaning memory");
-  //   Serial.println("[3] memory");
-  //   show_memory();
-    // free(buf);
-    // buf_len = 0;
-    //display.powerDown();
-  // }
-  Serial.println("[4] memory");
-  show_memory();
+  Serial.println("loop");
   delay(10000);
 }
-
-esp_err_t take_photo(uint8_t ** out, size_t * out_len) {
-  camera_fb_t *fb = NULL;
-  fb = esp_camera_fb_get();
-  if (!fb) {
-      Serial.println("Camera capture failed");
-      return ESP_FAIL;
-  }
-
-  if (fb->format == PIXFORMAT_GRAYSCALE || fb->format == PIXFORMAT_JPEG) {
-    Serial.println("format: PIXFORMAT_JPEG || PIXFORMAT_GRAYSCALE");
-    if(frame2bmp(fb, out, out_len)) {
-      Serial.println("conversion frame2bmp done");
-    }
-    esp_camera_fb_return(fb);
-  } else {
-    Serial.println("format: UNKNOWN");
-    esp_camera_fb_return(fb);
-    return ESP_FAIL;
-  }
-
-  return 0;
-}
-
-// void setup_wifi() {
-//   // We start by connecting to a WiFi network
-//   Serial.println();
-//   Serial.print("Connecting to ");
-//   Serial.println(ssid);
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin(ssid, password);
-//   WiFi.setSleep(false);
-
-//   uint32_t time1, time2;
-//   time1 = millis();
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//     time2 = millis();
-//     if ((time2 - time1) > 1000 * WIFI_CONNECT_TIMEOUT_S) { // wifi connection lasts too long
-//       break;
-//       ESP.restart();
-//     }
-//     if (time1 > time2)
-//       time1 = 0;
-//   }
-//   Serial.println("");
-//   Serial.println("WiFi connected");
-//   Serial.println("IP address: ");
-//   Serial.println(WiFi.localIP());
-// }
 
 void init_camera(void) {
   camera_config_t config;
@@ -205,7 +119,7 @@ void init_camera(void) {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_240X240;
+  config.frame_size = FRAMESIZE_VGA;
   config.pixel_format = PIXFORMAT_JPEG; // PIXFORMAT_JPEG | PIXFORMAT_GRAYSCALE; // for streaming
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
@@ -221,7 +135,7 @@ void init_camera(void) {
       config.grab_mode = CAMERA_GRAB_LATEST;
     } else {
       // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_240X240;
+      config.frame_size = FRAMESIZE_VGA;
       config.fb_location = CAMERA_FB_IN_DRAM;
     }
   } else {
@@ -254,30 +168,65 @@ void init_camera(void) {
   setupLedFlash(LED_GPIO_NUM);
 
   //s->set_framesize(s, FRAMESIZE_SVGA);
-  s->set_framesize(s, FRAMESIZE_240X240);
+  s->set_framesize(s, FRAMESIZE_VGA);
   enable_led(false);
+}
+
+esp_err_t take_photo(uint8_t ** out, size_t * out_len) {
+  camera_fb_t *fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
+      Serial.println("Camera capture failed");
+      return ESP_FAIL;
+  }
+
+  if (fb->format == PIXFORMAT_GRAYSCALE || fb->format == PIXFORMAT_JPEG) {
+    Serial.println("format: PIXFORMAT_JPEG || PIXFORMAT_GRAYSCALE");
+    if(frame2bmp(fb, out, out_len)) {
+      Serial.println("conversion frame2bmp done");
+    }
+    esp_camera_fb_return(fb);
+  } else {
+    Serial.println("format: UNKNOWN");
+    esp_camera_fb_return(fb);
+    return ESP_FAIL;
+  }
+
+  return 0;
+}
+
+void setupLedFlash(int pin) 
+{
+    ledcSetup(LED_LEDC_CHANNEL, 5000, 8);
+    ledcAttachPin(pin, LED_LEDC_CHANNEL);
+}
+
+void enable_led(bool en) { // Turn LED On or Off
+    int duty = en ? CONFIG_LED_MAX_INTENSITY : 0;
+    ledcWrite(LED_LEDC_CHANNEL, duty);
+    //ledc_set_duty(CONFIG_LED_LEDC_SPEED_MODE, CONFIG_LED_LEDC_CHANNEL, duty);
+    //ledc_update_duty(CONFIG_LED_LEDC_SPEED_MODE, CONFIG_LED_LEDC_CHANNEL);
+    Serial.print("Set LED intensity to ");Serial.println(duty);
 }
 
 static const uint16_t max_palette_pixels = 256; // for depth <= 8
 
 // const char* host, const char* path, const char* filename, int16_t x, int16_t y, bool with_color
-void drawBitmap2(uint8_t *bmp, size_t bmp_len) {
-  int16_t x = (display.width() - 240) / 2;
-  int16_t y = (display.height() - 240) / 2;
+void drawBitmap(uint8_t *bmp, size_t bmp_len) {
+  int16_t x = (display.width() - 640) / 2;
+  int16_t y = (display.height() - 480) / 2;
   if ((x >= display.width()) || (y >= display.height())) return;
   
   uint8_t mono_palette_buffer[max_palette_pixels / 8]; 
-  //uint8_t color_palette_buffer[max_palette_pixels / 8]; // palette buffer for depth <= 8 c/w
-
-  Serial.println("[drawBitmap] memory");
-  show_memory();
+  uint8_t color_palette_buffer[max_palette_pixels / 8]; // palette buffer for depth <= 8 c/w
 
   uint32_t startTime = millis();
   bool valid = false; // valid format to be handled
   bool flip = true; // bitmap is stored bottom-to-top
   size_t start = 0;
  
-  bool with_color = false;
+  bool with_color = true;
+  display.firstPage();
   display.fillScreen(GxEPD_WHITE);
 
   // Parse BMP header
@@ -326,13 +275,11 @@ void drawBitmap2(uint8_t *bmp, size_t bmp_len) {
           red   = read8(bmp, bmp_len, start);
           read8(bmp, bmp_len, start);
           whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
-          //colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+          colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
           if (0 == pn % 8) mono_palette_buffer[pn / 8] = 0;
           mono_palette_buffer[pn / 8] |= whitish << pn % 8;
-          //!if (0 == pn % 8) color_palette_buffer[pn / 8] = 0;
-          //!color_palette_buffer[pn / 8] |= colored << pn % 8;
-          //Serial.print("0x00"); Serial.print(red, HEX); Serial.print(green, HEX); Serial.print(blue, HEX);
-          //Serial.print(" : "); Serial.print(whitish); Serial.print(", "); Serial.println(colored);
+          if (0 == pn % 8) color_palette_buffer[pn / 8] = 0;
+          color_palette_buffer[pn / 8] |= colored << pn % 8;
         }
       }
       display.fillScreen(GxEPD_WHITE);
@@ -377,7 +324,7 @@ void drawBitmap2(uint8_t *bmp, size_t bmp_len) {
                 }
                 uint16_t pn = (in_byte >> bitshift) & bitmask;
                 whitish = mono_palette_buffer[pn / 8] & (0x1 << pn % 8);
-                //!colored = color_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                colored = color_palette_buffer[pn / 8] & (0x1 << pn % 8);
                 in_byte <<= depth;
                 in_bits -= depth;
               }
@@ -385,27 +332,28 @@ void drawBitmap2(uint8_t *bmp, size_t bmp_len) {
           }
           if (whitish) {
             color = GxEPD_WHITE;
-          //!} else if (colored && with_color) {
-          //!  color = GxEPD_RED;
+          } else if (colored && with_color) {
+            color = GxEPD_COLORED;
           } else {
             color = GxEPD_BLACK;
           }
           uint16_t yrow = y + (flip ? h - row - 1 : row);
           display.drawPixel(x + col, yrow, color);
           
-          if(color == GxEPD_BLACK) {
-            Serial.print("X");
-          } else {
-            Serial.print(" ");
-          }  
+          // if(color == GxEPD_BLACK) {
+          //   Serial.print("X");
+          // } else {
+          //   Serial.print(" ");
+          // }  
           //Serial.print(x + col);Serial.print("x");Serial.println(yrow);
         } // end pixel
-        Serial.println();
+        //Serial.println();
       } // end line
     }
     Serial.print("bytes read (start): "); Serial.println(start);
     Serial.print("loaded in "); Serial.print(millis() - startTime); Serial.println(" ms");
-    display.update();
+    while(display.nextPage());
+    // display.update();
   }  
   if (!valid) {
     Serial.println("bitmap format not handled.");
@@ -446,7 +394,8 @@ int32_t read32(uint8_t buf[], size_t buf_len, size_t &start) {
   return result;
 }
 
-void show_memory() {
+void show_memory(char arr[]) {
+  Serial.println(arr);
   Serial.print("Total heap: ");Serial.println(ESP.getHeapSize());
   Serial.print("Free heap: ");Serial.println(ESP.getFreeHeap());
   Serial.print("Total PSRAM: ");Serial.println(ESP.getPsramSize());
